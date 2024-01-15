7893543b21ed484c6d1a59036193df5b663d65ad
u.vakhidov <u.vakhidov@fincube.uz>
2024-01-15 18:29:11 +0500

    add identity

---
+++ src/Identity/Identity.Api/Controllers/UserController.cs
@@ -0,0 +1,16 @@
+﻿using EAnalytics.Common.Primitives;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Identity.Api.Controllers
+{
+    public class UserController : BaseApiController
+    {
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return Ok("");
+        }
+    }
+}
